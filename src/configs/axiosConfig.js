a25379c11569ab6a873db56960f21391684e1444
rodrilang <rodrigolang90mail.com>
2025-03-16 22:19:20 -0300

    change baseURL in axios

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Crear instancia de Axios con configuración base
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  //baseURL: "http://localhost:8080",
+  baseURL: "https://7ecb-190-189-40-246.ngrok-free.app",
   headers: {
     "Content-Type": "application/json", // Formato JSON por defecto
   },
